48297d93145606b8299d5cb595d3d4f28accaa1a
ngalkov <ngalkov@users.noreply.github.com>
2023-06-12 08:40:17 +0300

    Solve HW2_wandb

--- preprocess_data.py
+++ preprocess_data.py
@@ -3,6 +3,8 @@ import pickle
 import click
 import pandas as pd
 
+import wandb
+
 from sklearn.feature_extraction import DictVectorizer
 
 
@@ -14,21 +16,21 @@ def dump_pickle(obj, filename: str):
 def read_dataframe(filename: str):
     df = pd.read_parquet(filename)
 
-    df['duration'] = df['lpep_dropoff_datetime'] - df['lpep_pickup_datetime']
+    df["duration"] = df["lpep_dropoff_datetime"] - df["lpep_pickup_datetime"]
     df.duration = df.duration.apply(lambda td: td.total_seconds() / 60)
     df = df[(df.duration >= 1) & (df.duration <= 60)]
 
-    categorical = ['PULocationID', 'DOLocationID']
+    categorical = ["PULocationID", "DOLocationID"]
     df[categorical] = df[categorical].astype(str)
 
     return df
 
 
 def preprocess(df: pd.DataFrame, dv: DictVectorizer, fit_dv: bool = False):
-    df['PU_DO'] = df['PULocationID'] + '_' + df['DOLocationID']
-    categorical = ['PU_DO']
-    numerical = ['trip_distance']
-    dicts = df[categorical + numerical].to_dict(orient='records')
+    df["PU_DO"] = df["PULocationID"] + "_" + df["DOLocationID"]
+    categorical = ["PU_DO"]
+    numerical = ["trip_distance"]
+    dicts = df[categorical + numerical].to_dict(orient="records")
     if fit_dv:
         X = dv.fit_transform(dicts)
     else:
@@ -37,15 +39,22 @@ def preprocess(df: pd.DataFrame, dv: DictVectorizer, fit_dv: bool = False):
 
 
 @click.command()
+@click.option("--wandb_project", help="Name of Weights & Biases project")
+@click.option("--wandb_entity", help="Name of Weights & Biases entity")
 @click.option(
-    "--raw_data_path",
-    help="Location where the raw NYC taxi trip data was saved"
+    "--raw_data_path", help="Location where the raw NYC taxi trip data was saved"
 )
-@click.option(
-    "--dest_path",
-    help="Location where the resulting files will be saved"
-)
-def run_data_prep(raw_data_path: str, dest_path: str, dataset: str = "green"):
+@click.option("--dest_path", help="Location where the resulting files will be saved")
+def run_data_prep(
+    wandb_project: str,
+    wandb_entity: str,
+    raw_data_path: str,
+    dest_path: str,
+    dataset: str = "green",
+):
+    # Initialize a Weights & Biases run
+    wandb.init(project=wandb_project, entity=wandb_entity, job_type="preprocess")
+
     # Load parquet files
     df_train = read_dataframe(
         os.path.join(raw_data_path, f"{dataset}_tripdata_2022-01.parquet")
@@ -58,7 +67,7 @@ def run_data_prep(raw_data_path: str, dest_path: str, dataset: str = "green"):
     )
 
     # Extract the target
-    target = 'tip_amount'
+    target = "tip_amount"
     y_train = df_train[target].values
     y_val = df_val[target].values
     y_test = df_test[target].values
@@ -78,6 +87,10 @@ def run_data_prep(raw_data_path: str, dest_path: str, dataset: str = "green"):
     dump_pickle((X_val, y_val), os.path.join(dest_path, "val.pkl"))
     dump_pickle((X_test, y_test), os.path.join(dest_path, "test.pkl"))
 
+    artifact = wandb.Artifact("NYC-Taxi", type="preprocessed_dataset")
+    artifact.add_dir(dest_path)
+    wandb.log_artifact(artifact)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     run_data_prep()
